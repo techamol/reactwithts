2df8f695d71718061dfe60cd76f0a93e94f7559b
techamol <tech.amolsontakke@gmail.com>
2025-04-29 14:26:05 +0530

    reuse name

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,18 +3,14 @@ type inputProps = {
   value: string;
   handleChnage: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
-export default function Input(props: inputProps) {
+export default function Input({ value, handleChnage }: inputProps) {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event);
   };
 
   return (
     <div>
-      <input
-        type="text"
-        value={props.value}
-        onChange={handleInputChange}
-      ></input>
+      <input type="text" value={value} onChange={handleInputChange}></input>
     </div>
   );
 }
